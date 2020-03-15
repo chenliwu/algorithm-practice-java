9cb0ee430d9358698e1dfeadaa67d5218487f792
chenlw <30075213@qq.com>
2020-03-15 23:38:05 +0800

    update

--- src/Solution.java
+++ src/Solution.java
@@ -1,18 +1,8 @@
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- * int val;
- * TreeNode left;
- * TreeNode right;
- * TreeNode(int x) { val = x; }
- * }
- */
-
 class TreeNode {
-    int val;
+    int val = 0;
     TreeNode left;
     TreeNode right;
 
@@ -22,29 +12,13 @@ class TreeNode {
 }
 
 public class Solution {
-    List<List<Integer>> resultList = new ArrayList<>();
 
-    public List<List<Integer>> levelOrder(TreeNode root) {
-        helper(root, 0);
-        return resultList;
+    public static void main(String[] args) {
+        System.out.println(new Solution().sumNums(100));
     }
 
-    private void helper(TreeNode root, int level) {
-        if (root == null)
-            return;
-        if (resultList.size() == level) {
-            // 初始化层次节点的存储列表
-            resultList.add(new ArrayList<>());
-        }
-        if (level % 2 == 0) {
-            // 从左右往右遍历
-            resultList.get(level).add(root.val);
-        } else {
-            // 从右往左遍历
-            resultList.get(level).add(0, root.val);
-        }
-        helper(root.left, level + 1);
-        helper(root.right, level + 1);
+    public int sumNums(int n) {
+        return n * (n + 1) / 2;
     }
 
 }
